57c4929d9ab45bbb879caea2f5a87662f61b8494
jiyanfei <1414669306@qq.com>
2017-07-21 10:45:28 +0800

    侧滑菜单有些文件以前没提交上

---
+++ app/src/main/java/com/a/quarter/view/activity/follow/HotFollowActivity.java
@@ -0,0 +1,23 @@
+package com.a.quarter.view.activity.follow;
+
+import com.a.quarter.R;
+import com.a.quarter.view.base.BaseActivity;
+
+public class HotFollowActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_hot_follow;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+
+}
---
+++ app/src/main/java/com/a/quarter/view/activity/follow/MyFollowActivity.java
@@ -0,0 +1,26 @@
+package com.a.quarter.view.activity.follow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.a.quarter.R;
+import com.a.quarter.view.base.BaseActivity;
+
+/**我的关注页面*/
+public class MyFollowActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_my_follow;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initDatas() {
+
+    }
+}
