0b9eae7afb719ca322eaa63afbdc9c1c7a976924
wwxxff <3196554078@qq.com>
2017-07-25 17:31:04 +0800

    adapter

--- app/src/main/java/com/a/quarter/view/fragment/recommend/HotFragment.java
+++ app/src/main/java/com/a/quarter/view/fragment/recommend/HotFragment.java
@@ -2,14 +2,20 @@ package com.a.quarter.view.fragment.recommend;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import com.a.quarter.R;
+import com.a.quarter.view.adapter.recommend.HotAdapter;
 import com.a.quarter.view.base.BaseFragment;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,8 +33,10 @@ public class HotFragment extends BaseFragment {
     LinearLayout linearlayout;
     @Bind(R.id.recommand_fragemnt_recyclerview)
     RecyclerView recyclerview;
+    @Bind(R.id.scroller)
+    ScrollView scroller;
+    private float scaleY;
 
-    
     @Override
     protected int getContentViewId() {
         return R.layout.frag_hot;
@@ -40,8 +48,38 @@ public class HotFragment extends BaseFragment {
     }
 
     @Override
-    protected void initDatas() {
-
+    public void onResume() {
+        super.onResume();
+//        viewpager.setFocusable(true);
+    scroller.smoothScrollTo(0,0);
     }
 
+    @Override
+    protected void initDatas() {
+        recyclerview.setLayoutManager(new LinearLayoutManager(mActivity));
+
+        HotAdapter hotAdapter = new HotAdapter(mActivity);
+        recyclerview.setAdapter(hotAdapter);
+
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i <5 ; i++) {
+            strings.add("2029-0"+i);
+        }
+
+        hotAdapter.setData(strings);
+        hotAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+
+        if (hidden){
+            scaleY = scroller.getScaleY();
+
+            Log.i("hidden",scaleY+"");
+        }else {
+            scroller.smoothScrollTo(0, (int) scaleY);
+        }
+    }
 }
