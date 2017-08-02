365459770785aaaf8c0e532194732d2a004a7599
jiyanfei <1414669306@qq.com>
2017-08-02 15:14:39 +0800

    修改推荐页面视频列表

--- app/src/main/java/com/a/quarter/view/fragment/recommend/HotFragment.java
+++ app/src/main/java/com/a/quarter/view/fragment/recommend/HotFragment.java
@@ -58,72 +58,7 @@ public class HotFragment extends BaseFragment {
         list = new ArrayList<ContentListBean>();
         rv.setAdapter(adapter = new ContentListAdapter(getActivity(), list));
 
-        //监听列表的滚动
-//        rv.addOnScrollListener(new RecyclerView.OnScrollListener() {
-//
-//            @Override
-//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-//                super.onScrolled(recyclerView, dx, dy);
-//            }
-//
-//            @Override
-//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-//                super.onScrollStateChanged(recyclerView, newState);
-//            }
-//
-//        });
-//        rv.setRecyclerListener(new RecyclerView.RecyclerListener() {
-//
-//            @Override
-//            public void onViewRecycled(RecyclerView.ViewHolder holder) {
-//                if(holder instanceof ContentListAdapter.VideoViewHolder){
-//
-//                }
-//            }
-//
-//        });
-
-//        onChildAttachStateChangeListener =
-//                new RecyclerView.OnChildAttachStateChangeListener(){
-//
-//            ViewGroup parent = null;
-//                    @Override
-//            public void onChildViewAttachedToWindow(View view) {
-//
-//            }
-//
-//            //在条目从屏幕上消失的时候，如果是视频条目，移除video控件，恢复未开始的状态
-//            //这个方法有效
-//            @Override
-//            public void onChildViewDetachedFromWindow(View view) {
-////                RecyclerView.LayoutParams p = (RecyclerView.LayoutParams)view.getLayoutParams();
-////                p.
-//
-//                LogUtils.i("onChildViewDetachedFromWindow,view="+view);
-//                if(adapter.player!=null) {
-//                    parent = (ViewGroup)adapter.player.getParent();
-//                    if (parent!=null){
-//                        if(parent.getParent() == view){
-//                            LogUtils.i("移除视频控件,view="+view);
-//                            adapter.player.stopPlayback();
-//                            adapter.player.release(true);
-//                            parent.removeView(adapter.player);
-//                            //所有条目只使用一个IjkVideoView，解决remove之后可能会出现上个播放视频的条目在视频的位置一片空白的情况
-//
-//                            //TODO 显示开始播放的图标和视频缩略图
-//                            //怎么找到 VideoViewHolder 对象？
-//                            rv.getChildViewHolder(view);
-//                            rv.findViewHolderForAdapterPosition()
-//                        }
-//                    }
-//                }
-////                if(list.get()rv.getChildAdapterPosition(view)){
-////
-////                }
-//            }
-//
-//        };
-
+        //监听条目的消失
         onChildAttachStateChangeListener =
         new RecyclerView.OnChildAttachStateChangeListener(){
 
